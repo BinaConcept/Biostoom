df068c2627d9fc27a776e42995262cfc683207cf
Arslan Bulent <info.hecasolutions@gmail.com>
2022-08-16 22:40:00 +0200

    master loading

---
+++ src/main/java/be/biostoom/certificate/BiostoomApplication.java
@@ -0,0 +1,13 @@
+package be.biostoom.certificate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiostoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiostoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/biostoom/certificate/BiostoomApplicationTests.java
@@ -0,0 +1,13 @@
+package be.biostoom.certificate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiostoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
