33602652d1d4b3096536a8daee7b3cb37a42ea3b
atifkhan <atifkhan23490@gmail.com>
2024-03-15 14:28:25 +0100

    first commit

--- src/main/java/be/biostoom/certificate/repository/PermitRepository.java
+++ src/main/java/be/biostoom/certificate/repository/PermitRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface PermitRepository extends PagingAndSortingRepository<Permit, Long>, PagingSpecificationExecutor<Permit, Long>, ReferenceDao<Permit, Long> {
